21ca65963e3afa4c025737071b72f85f14efa81a
Aeun-mani-namdev <0126it213d02@oriental.ac.in>
2025-07-25 17:18:21 +0530

    added index

---
+++ Day-01/style.js
@@ -0,0 +1,37 @@
+console.log("hello world");
+
+alert("Arun mani Namdev");
+
+let name = "arun mani namdev";
+
+console.log(name);
+
+let age = 22;
+
+console.log(age);
+
+
+age = 24;
+console.log(age +2);
+
+let name1 = "Aditya";
+age = 23;
+city ="indore";
+price = 1000.05;
+
+// x=null;
+// y=undefined;
+// console.log(x);
+// console.log(y);
+
+console.log(name1, age, city, price);
+
+
+let x;
+console.log(x);
+
+let y = null;
+console.log(y);
+
+name = 56;
+console.log(name);
